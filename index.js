6c9733d52744ce449df712e044ed3c43138a838c
Rob Walton <robwalton06@gmail.com>
2023-02-28 12:57:16 +0000

    Ability to run server added

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
-
+const ejs = require('ejs');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-const db = mysql.createConnection({
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'products'
 });
 
-app.listen(3001, () => {
-  console.log('server running on port 3001');
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
 });
