3b32a9fc762b134fcd6c1cfd9861578f9d8bb477
Rob Walton <robwalton06@gmail.com>
2023-03-02 18:39:16 +0000

    Tailwind and daisy ui fully working

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express();
 const ejs = require('ejs');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const path = require("path");
 
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -24,6 +29,6 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server running on port 3000');
 });
