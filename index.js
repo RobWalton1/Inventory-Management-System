0be8d0bc548f4ae53e0f7e26036337cd5b937e8d
Rob Walton <robwalton06@gmail.com>
2023-03-24 16:28:36 +0000

    Items low on stock now displaying

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ connection.connect((err) => {
     console.log('Connected!');
 });
 
-
 //Routes
 app.get('/', (req, res) => {
     res.render('index');
@@ -35,8 +34,12 @@ app.get('index', (req, res) => {
     res.render('index');
 });
 
+//Selecting the top 3 products that are low on quantity
 app.get('/home', (req, res) => {
-    res.render('home');
+    connection.query('SELECT * FROM productstable ORDER BY Quantity ASC LIMIT 3 OFFSET 0', function(error, results, fields) {
+        if (error) throw error;
+        res.render('home', { products: results });
+      });
 });
 
 app.get('/addProducts', (req, res) => {
@@ -49,6 +52,10 @@ app.get('/deleteProducts', (req, res) => {
 
 app.get('/search', (req, res) => {
     res.render('search');
+});
+
+app.get('/searchResults', (req, res) => {
+    res.render('searchResults');
 });
 
 app.listen(port, () => {
@@ -70,16 +77,32 @@ const createPool = mysql.createPool({
 //form submission to add products
 app.post('/submit-form', (req, res) => {
     const { ID, productName, Description, Quantity, Location, Price } = req.body;
-
     createPool.query('INSERT INTO productstable (ID, productName, Description, Quantity, Location, Price) VALUES (?, ?, ?, ?, ?, ?)', [ID, productName, Description, Quantity, Location, Price || 1], (err, results) => {
         if (err) {
             console.error(err);
             res.status(500).send('Product ID in use');
         } else {
             console.log(results);
-            res.send('Product added successfully');
+            res.redirect('/addProducts');
         }
     });
 });
+
+//Searching for a product by ID, contains parameterized query to prevent SQL injection
+app.post('/search', (req, res) => {
+    const searchTerm = req.body.searchTerm;
+    const searchQuery = 'SELECT * FROM productstable WHERE ID = ?';
+    connection.query(searchQuery, [searchTerm], (error, results, fields) => {
+      if (error) throw error;
+      console.log('Rendering searchResults.ejs file');
+      res.render('searchResults', { results });
+
+    });
+  });
+
+
+//Deleting a product by ID, contains parameterized query to prevent SQL injection
+
+
 
 
